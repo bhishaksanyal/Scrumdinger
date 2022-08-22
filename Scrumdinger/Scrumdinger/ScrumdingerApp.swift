3166716ff836e9bc4da4613235158976d5a4f3d8
bhishaksanyal <sanyalbishak93@gmail.com>
2022-08-22 20:12:16 +0530

    Initial commit

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Bhishak Sanyal on 22/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
