28c75bce4539885a585ab4002f583a1601092bd7
bhishaksanyal <sanyalbishak93@gmail.com>
2022-10-08 18:13:12 +0530

    Navigation setup added

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -28,7 +28,7 @@ enum Theme: String {
     
     var accentColor: Color {
         switch self {
-        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .white
         case .indigo, .magenta, .navy, .oxblood, .purple: return .white
         }
     }
